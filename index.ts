d2c70723fae74988a456ab7b43a9ab9515231967
andres velasco <velasco.andrs@gmail.com>
2020-05-15 11:41:31 -0500

    configuracion del tsconfig.json y el package.json

--- index.ts
+++
@@ -1,5 +0,0 @@
-export {crearDatos} from './src/funciones/archivos/cargar-datos-prueba';
-export {borrarArhicvo} from './src/funciones/archivos/borrar-archivos';
-export {ControladorPrincipal} from './src/clases-genericas/controlador.principal';
-export {ServicioPrincipal} from './src/clases-genericas/servicio.principal';
-export {EntidadPrincipal} from './src/clases-genericas/entidad.principal';
