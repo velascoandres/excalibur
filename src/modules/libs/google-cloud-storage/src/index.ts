7b0280fc002a35a3ca323456795dbc7e729eed04
andres velasco <velasco.andrs@gmail.com>
2020-05-23 23:56:36 -0500

    exporto interceptor

--- src/modules/libs/google-cloud-storage/src/index.ts
+++ src/modules/libs/google-cloud-storage/src/index.ts
@@ -1,2 +1,3 @@
 export * from './google-cloud-storage.module';
 export * from './google-cloud-storage.service';
+export {GoogleCloudStorageFileInterceptor} from './interceptors/google-cloud-storage-file-interceptor';
