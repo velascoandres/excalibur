9dcffc1bd0df2eb15f5df51c1a4d2562a8532c8d
andres velasco <velasco.andrs@gmail.com>
2020-06-15 01:19:03 -0500

    update index

--- src/index.ts
+++ src/index.ts
@@ -10,19 +10,28 @@ export {ApiDoc} from './api/decorators/swagger-crud/swagger-crud.decorator';
 export {CrudApiConfig} from './api/decorators/swagger-crud/interfaces';
 export {findFull} from './api/funciones/busqueda/busqueda-compuesta/find-full.funcion';
 export {findFullTransaccion} from './api/funciones/busqueda/busqueda-compuesta/find-full.transaccion.funcion';
-export {ConsultaFindFullInterface} from './api/funciones/busqueda/busqueda-compuesta/interfaces/consulta.findFull.interface';
+export {
+    ConsultaFindFullInterface
+} from './api/funciones/busqueda/busqueda-compuesta/interfaces/consulta.findFull.interface';
 export {CuerpoConsultaFindFull} from './api/funciones/busqueda/busqueda-compuesta/interfaces/cuerpo.consulta.findFull';
 export {OrderByInterface} from './api/funciones/busqueda/busqueda-compuesta/interfaces/orderBy.interface';
-export {OperadorConsultaInterface} from './api/funciones/busqueda/busqueda-compuesta/interfaces/operador.consulta.interface';
-export {OperadorConsultaSimpleInterface} from './api/funciones/busqueda/busqueda-compuesta/interfaces/operador.consulta.simple.interface';
+export {
+    OperadorConsultaInterface
+} from './api/funciones/busqueda/busqueda-compuesta/interfaces/operador.consulta.interface';
+export {
+    OperadorConsultaSimpleInterface
+} from './api/funciones/busqueda/busqueda-compuesta/interfaces/operador.consulta.simple.interface';
 export {PrincipalMongoService} from './api/principal/services/principal.mongo.service';
 export {ServiceCrudMethodsInterface} from './api/interfaces/service.crud.methods.interfaces';
 export {MongoServiceCrudMethodsInterface} from './api/interfaces/service.crud.methods.interfaces';
 export {MongoIndexConfigInterface} from './api/interfaces/service.crud.methods.interfaces';
-export {PrincipalMongoController} from  './api/principal/controllers/principal.mongo.controller';
-export {PrincipalMongoUpdateDto} from  './api/principal/schemas/principal.mongo.update.dto';
-export {PrincipalMongoEntity} from  './api/principal/schemas/principal.mongo.entity';
-export  *  from  './api/interfaces/controllers.interfaces';
+export {PrincipalMongoController} from './api/principal/controllers/principal.mongo.controller';
+export {PrincipalMongoUpdateDto} from './api/principal/schemas/principal.mongo.update.dto';
+export {PrincipalMongoEntity} from './api/principal/schemas/principal.mongo.entity';
+export * from './api/interfaces/controllers.interfaces';
+export {GENERIC_TYPE_VALIDATOR} from './api/decorators/cutom-validator/constants';
+export {IsType} from './api/decorators/cutom-validator/isType';
+export {IsTypeDirect} from './api/decorators/cutom-validator/isTypeDirect';
 // MODULOS ************************************************************
 export * from './modules/libs/google-cloud-storage/src/index';
 export * from './modules/libs/firebase/src/index';
