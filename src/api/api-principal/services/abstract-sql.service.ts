672e793fed05b41688aa8167a951fa38198a8658
andres velasco <velasco.andrs@gmail.com>
2020-09-19 00:28:43 -0500

    crud Methods with transactions on abs sql service

--- src/api/api-principal/services/abstract-sql.service.ts
+++ src/api/api-principal/services/abstract-sql.service.ts
@@ -1,4 +1,4 @@
-import { EntityManager } from 'typeorm';
+import { DeepPartial, EntityManager, FindConditions, ObjectID } from 'typeorm';
 import { FindFullQuery } from '../../..';
 import { AbstractService } from './abstract.service';
 import { findFullTransaccion } from '../../functions/search/complex-search/find-full.transaction.function';
@@ -8,5 +8,40 @@ export abstract class AbstractSQLService<Entity> extends AbstractService<Entity>
     async findAllWithTransaction(entityManager: EntityManager, query: FindFullQuery) {
         const tableName: string = this._repository.metadata.tableName;
         return await findFullTransaccion<Entity>(entityManager, tableName, query as FindFullQuery);
+    }
+
+    async findOneWithTransaction(
+        entityManager: EntityManager,
+        criteria: string | string[] | number | number[] | Date | Date[] | ObjectID | ObjectID[] | FindConditions<Entity>,
+        record: DeepPartial<Entity>
+    ) {
+        const tableName: string = this._repository.metadata.tableName;
+        const repository = entityManager.getRepository(tableName);
+        return await repository.findOne(criteria, record);
+    }
+
+    async updateOneWithTransaction(
+        entityManager: EntityManager,
+        criteria: string | string[] | number | number[] | Date | Date[] | ObjectID | ObjectID[] | FindConditions<Entity>,
+        record: DeepPartial<Entity>
+    ) {
+        const tableName: string = this._repository.metadata.tableName;
+        const repository = entityManager.getRepository(tableName);
+        return await repository.update(criteria, record);
+    }
+
+    async createOneWithTransaction(entityManager: EntityManager, newRecord: DeepPartial<Entity>) {
+        const tableName: string = this._repository.metadata.tableName;
+        const repository = entityManager.getRepository(tableName);
+        return await repository.save(newRecord);
+    }
+
+    async deleteOneWithTransaction(
+        entityManager: EntityManager,
+        criteria: string | string[] | number | number[] | Date | Date[] | ObjectID | ObjectID[] | FindConditions<Entity>,
+    ) {
+        const tableName: string = this._repository.metadata.tableName;
+        const repository = entityManager.getRepository(tableName);
+        return await repository.delete(criteria);
     }
 }
